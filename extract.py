542e6a606be55135c40b32ded672475ab98c8aea
Craig Stephenson <crstephenson@alaska.edu>
2021-11-04 12:13:35 -0800

    Move constants and lookup tables into luts.py file.

--- extract.py
+++ extract.py
@@ -10,6 +10,7 @@ import os
 import logging
 import multiprocessing as mp
 from functools import partial
+import luts
 
 logging.basicConfig(level=logging.INFO)
 
@@ -196,107 +197,15 @@ def process_dataset(communities, geotiffs, scenario, resolution, type, daterange
     return run_extraction(geotiffs, communities, scenario, resolution, type, daterange)
 
 if __name__ == '__main__':
-    alaska = pd.read_csv('../geospatial-vector-veracity/vector_data/point/alaska_point_locations.csv')
-    alberta = pd.read_csv('../geospatial-vector-veracity/vector_data/point/alberta_point_locations.csv')
-    british_columbia = pd.read_csv('../geospatial-vector-veracity/vector_data/point/british_columbia_point_locations.csv')
-    manitoba = pd.read_csv('../geospatial-vector-veracity/vector_data/point/manitoba_point_locations.csv')
-    nwt = pd.read_csv('../geospatial-vector-veracity/vector_data/point/northwest_territories_point_locations.csv')
-    saskatchewan = pd.read_csv('../geospatial-vector-veracity/vector_data/point/saskatchewan_point_locations.csv')
-    yukon = pd.read_csv('../geospatial-vector-veracity/vector_data/point/yukon_point_locations.csv')
-
-    all_locations = pd.concat([
-        alaska,
-        alberta,
-        british_columbia,
-        manitoba,
-        nwt,
-        saskatchewan,
-        yukon
-    ])
-
-    scenarios_lu = [
-        'cru32',
-        'prism',
-        'rcp45',
-        'rcp60',
-        'rcp85'
-    ]
-
-    communities_lu = {
-        'cru32': all_locations,
-        'prism': alaska,
-        'rcp45': all_locations,
-        'rcp60': all_locations,
-        'rcp85': all_locations
-    }
-
-    types_lu = {
-        'tas': 'Temperature',
-        'pr': 'Precipitation'
-    }
-
-    resolutions_lu = {
-        'cru32': [
-            '10min'
-        ],
-        'prism': [
-            '2km'
-        ],
-        'rcp45': [
-            '2km',
-            '10min'
-        ],
-        'rcp60': [
-            '2km',
-            '10min'
-        ],
-        'rcp85': [
-            '2km',
-            '10min'
-        ],
-    }
-
-    dateranges_lu = {
-        'cru32': [
-            [1960, 1989]
-        ],
-        'prism': [
-            [1961, 1990]
-        ],
-        'rcp45': [
-            [2040, 2049],
-            [2060, 2069],
-            [2090, 2099]
-        ],
-        'rcp60': [
-            [2040, 2049],
-            [2060, 2069],
-            [2090, 2099]
-        ],
-        'rcp85': [
-            [2040, 2049],
-            [2060, 2069],
-            [2090, 2099]
-        ]
-    }
-
-    projections_lu = {
-        'cru32': 'EPSG:4326',
-        'prism': 'EPSG:3338',
-        'rcp45': 'EPSG:3338',
-        'rcp60': 'EPSG:3338',
-        'rcp85': 'EPSG:3338'
-    }
-
-    for scenario in scenarios_lu:
-        for type in types_lu.keys():
-            for resolution in resolutions_lu[scenario]:
+    for scenario in luts.scenarios_lu:
+        for type in luts.types_lu.keys():
+            for resolution in luts.resolutions_lu[scenario]:
                 path = '{0}/{1}/{2}/'.format(scenario, resolution, type)
                 geotiffs = glob.glob(os.path.join(path, '*.tif'))
-                communities = communities_lu[scenario]
-                type_label = types_lu[type]
-                projection = projections_lu[scenario]
-                for daterange in dateranges_lu[scenario]:
+                communities = luts.communities_lu[scenario]
+                type_label = luts.types_lu[type]
+                projection = luts.projections_lu[scenario]
+                for daterange in luts.dateranges_lu[scenario]:
                     results = process_dataset(communities, geotiffs, scenario, resolution, type_label, daterange, projection)
                     keys = results[0].keys()
                     with open('data.csv', 'a', newline='') as output_file:
