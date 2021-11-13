c2a0a7aa860d7c6e7c39361af18eb5592a80bbf2
Craig Stephenson <crstephenson@alaska.edu>
2021-11-12 15:02:34 -0900

    Run code through Black formatter.

--- extract.py
+++ extract.py
@@ -17,20 +17,22 @@ import luts
 
 logging.basicConfig(level=logging.INFO)
 
+
 def get_rowcol_from_point(x, y, transform):
     col, row = ~transform * (x, y)
     col, row = int(col), int(row)
     return row, col
 
-def extract_data(filepath, communities, scenario, daterange):
-    filename = filepath.split('/')[-1]
-    filename_prefix = filename.split('.')[0]
-    filename_parts = filename_prefix.split('_')
 
-    if scenario == 'prism':
-        month = filename_parts[5].lstrip('0')
+def extract_data(filepath, communities, scenario, daterange):
+    filename = filepath.split("/")[-1]
+    filename_prefix = filename.split(".")[0]
+    filename_parts = filename_prefix.split("_")
+
+    if scenario == "prism":
+        month = filename_parts[5].lstrip("0")
     else:
-        month = filename_parts[6].lstrip('0')
+        month = filename_parts[6].lstrip("0")
         year = int(filename_parts[7])
         if year < daterange[0] or year > daterange[1]:
             return []
@@ -40,16 +42,15 @@ def extract_data(filepath, communities, scenario, daterange):
         data = []
         for index, community in communities.iterrows():
             value = get_closest_value(arr, community)
-            data.append({
-                'id': community['id'],
-                'month': month,
-                'value': value
-            })
+            data.append({"id": community["id"], "month": month, "value": value})
 
     return data
 
+
 def run_extraction(files, communities, scenario, resolution, type, daterange):
-    f = partial(extract_data, communities=communities, scenario=scenario, daterange=daterange)
+    f = partial(
+        extract_data, communities=communities, scenario=scenario, daterange=daterange
+    )
     pool = mp.Pool(8)
     extracted = pool.map(f, files)
     pool.close()
@@ -65,40 +66,42 @@ def run_extraction(files, communities, scenario, resolution, type, daterange):
     month_values = {}
     results = []
     for index, community in communities.iterrows():
-        month_values[community['id']] = {}
+        month_values[community["id"]] = {}
         for month in months:
-            month_values[community['id']][str(month)] = []
+            month_values[community["id"]][str(month)] = []
 
     for result in combined:
-        community_id = result['id']
-        month = str(result['month'])
-        value = result['value']
+        community_id = result["id"]
+        month = str(result["month"])
+        value = result["value"]
         month_values[community_id][month].append(value)
 
     for index, community in communities.iterrows():
         row = {
-            'id': community['id'],
-            'community': community['name'],
-            'region': community['region'],
-            'country': community['country'],
-            'latitude': community.loc['orig']['lat'],
-            'longitude': community.loc['orig']['lon'],
-            'type': type,
-            'scenario': scenario,
-            'resolution': resolution,
-            'unit': 'C'
+            "id": community["id"],
+            "community": community["name"],
+            "region": community["region"],
+            "country": community["country"],
+            "latitude": community.loc["orig"]["lat"],
+            "longitude": community.loc["orig"]["lon"],
+            "type": type,
+            "scenario": scenario,
+            "resolution": resolution,
+            "unit": "C",
         }
 
-        if scenario in ['cru32', 'prism']:
-            row['daterange'] = 'Historical'
+        if scenario in ["cru32", "prism"]:
+            row["daterange"] = "Historical"
         else:
-            row['daterange'] = '{0}-{1}'.format(daterange[0], daterange[1])
+            row["daterange"] = "{0}-{1}".format(daterange[0], daterange[1])
 
         data_exists = True
         for month in months:
-            month_abbr = datetime.datetime.strptime(str(month), "%m").strftime("%b").lower()
-            month_label_mean = month_abbr + 'Mean'
-            mean_values = np.array(month_values[community['id']][str(month)])
+            month_abbr = (
+                datetime.datetime.strptime(str(month), "%m").strftime("%b").lower()
+            )
+            month_label_mean = month_abbr + "Mean"
+            mean_values = np.array(month_values[community["id"]][str(month)])
 
             if None in mean_values:
                 data_exists = False
@@ -111,34 +114,28 @@ def run_extraction(files, communities, scenario, resolution, type, daterange):
 
     return results
 
+
 def project(x, projection):
     point = pyproj.Proj(projection)(x.longitude, x.latitude)
-    x['orig'] = {
-        'lat': x.latitude,
-        'lon': x.longitude
-    }
-    x['proj'] = {
-        'lat': point[1],
-        'lon': point[0]
-    }
+    x["orig"] = {"lat": x.latitude, "lon": x.longitude}
+    x["proj"] = {"lat": point[1], "lon": point[0]}
     return x
+
 
 def transform(x, meta, rowcol_offset):
-    lat = x['proj']['lat']
-    lon = x['proj']['lon']
-    row, col = get_rowcol_from_point(lon, lat, transform=meta['transform'])
+    lat = x["proj"]["lat"]
+    lon = x["proj"]["lon"]
+    row, col = get_rowcol_from_point(lon, lat, transform=meta["transform"])
 
-    x['rowcol'] = {
-        'row': row + rowcol_offset,
-        'col': col + rowcol_offset
-    }
+    x["rowcol"] = {"row": row + rowcol_offset, "col": col + rowcol_offset}
 
     return x
 
+
 def get_closest_value(arr, community):
-    rowcol = community.loc['rowcol']
-    row = rowcol['row']
-    col = rowcol['col']
+    rowcol = community.loc["rowcol"]
+    row = rowcol["row"]
+    col = rowcol["col"]
     value = arr[row][col]
     distance = 0
 
@@ -148,7 +145,7 @@ def get_closest_value(arr, community):
 
     # Check points around perimeter of previously checked points.
     # TODO: Ignore the innermost set of checked points to optimize considerably.
-    while np.isclose(value, -3.40E+38) or np.isclose(value, -9999.0):
+    while np.isclose(value, -3.40e38) or np.isclose(value, -9999.0):
         distance += 1
         if distance > 8:
             return None
@@ -178,86 +175,108 @@ def get_closest_value(arr, community):
                 checked[offset_row][offset_col] = True
 
                 value = arr[offset_row][offset_col]
-                if not np.isclose(value, -3.40E+38) and not np.isclose(value, -9999.0):
+                if not np.isclose(value, -3.40e38) and not np.isclose(value, -9999.0):
                     return value
 
     return value
 
-def process_dataset(scenario, resolution, type_label, daterange, geotiffs, communities, projection):
+
+def process_dataset(
+    scenario, resolution, type_label, daterange, geotiffs, communities, projection
+):
     with rasterio.open(geotiffs[0]) as tmp:
-    	meta = tmp.meta
+        meta = tmp.meta
     communities = communities.apply(project, projection=projection, axis=1)
 
     rowcol_offset = 0
-    if scenario in ['rcp45', 'rcp60', 'rcp85']:
-        if resolution == '10min':
+    if scenario in ["rcp45", "rcp60", "rcp85"]:
+        if resolution == "10min":
             rowcol_offset = -1
 
-    communities = communities.apply(transform, meta=meta, rowcol_offset=rowcol_offset, axis=1)
-    return run_extraction(geotiffs, communities, scenario, resolution, type_label, daterange)
+    communities = communities.apply(
+        transform, meta=meta, rowcol_offset=rowcol_offset, axis=1
+    )
+    return run_extraction(
+        geotiffs, communities, scenario, resolution, type_label, daterange
+    )
+
 
 def process_scenarios(scenarios):
     for scenario in luts.scenarios_lu:
         process_resolutions(scenario, luts.resolutions_lu[scenario])
 
+
 def process_resolutions(scenario, resolutions):
     for resolution in resolutions:
         process_types(scenario, resolution, luts.types_lu)
 
+
 def process_types(scenario, resolution, types):
     for type in types:
-        path = 'input/{0}/{1}/{2}/'.format(scenario, resolution, type)
-        geotiffs = glob.glob(os.path.join(path, '*.tif'))
+        path = "input/{0}/{1}/{2}/".format(scenario, resolution, type)
+        geotiffs = glob.glob(os.path.join(path, "*.tif"))
         projection = luts.projections_lu[scenario]
         type_label = luts.types_lu[type]
-        process_dateranges(scenario, resolution, type, type_label, luts.dateranges_lu[scenario], geotiffs)
+        process_dateranges(
+            scenario,
+            resolution,
+            type,
+            type_label,
+            luts.dateranges_lu[scenario],
+            geotiffs,
+        )
+
 
 def create_csv(filename, keys):
-    with open(filename, 'a', newline='') as output_file:
+    with open(filename, "a", newline="") as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         output_file.close()
+
 
 def process_dateranges(scenario, resolution, type, type_label, dateranges, geotiffs):
     communities = luts.communities_lu[scenario]
     projection = luts.projections_lu[scenario]
     for daterange in dateranges:
-        results = process_dataset(scenario, resolution, type_label, daterange, geotiffs, communities, projection)
+        results = process_dataset(
+            scenario,
+            resolution,
+            type_label,
+            daterange,
+            geotiffs,
+            communities,
+            projection,
+        )
         keys = results[0].keys()
 
         for index, community in communities.iterrows():
-            filename = 'output/' + community['id'] + '.csv'
+            filename = "output/" + community["id"] + ".csv"
             if not os.path.exists(filename):
                 create_csv(filename, keys)
 
             data = []
             for result in results:
-                if result['id'] == community['id']:
+                if result["id"] == community["id"]:
                     data.append(result)
 
-            with open(filename, 'a', newline='') as output_file:
+            with open(filename, "a", newline="") as output_file:
                 dict_writer = csv.DictWriter(output_file, keys)
                 dict_writer.writerows(data)
                 output_file.close()
 
-        log_vars = [
-            scenario,
-            resolution,
-            type,
-            daterange[0],
-            daterange[1]
-        ]
+        log_vars = [scenario, resolution, type, daterange[0], daterange[1]]
 
-        logging.info('Complete: {0}/{1}/{2}/{3}-{4}'.format(*log_vars))
+        logging.info("Complete: {0}/{1}/{2}/{3}-{4}".format(*log_vars))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     locations = luts.all_locations
     communities = {}
     for index, location in locations.iterrows():
-        communities[location['id']] = location['name'] + ', ' + location['region']
+        communities[location["id"]] = location["name"] + ", " + location["region"]
 
     # Output the file used to populate the web app community selector dropdown.
-    community_file = open('CommunityNames.json', 'w')
+    community_file = open("CommunityNames.json", "w")
     json.dump(communities, community_file)
     community_file.close()
 
--- luts.py
+++ luts.py
@@ -1,102 +1,67 @@
 import pandas as pd
 
-alaska = pd.read_csv('../geospatial-vector-veracity/vector_data/point/alaska_point_locations.csv')
-alberta = pd.read_csv('../geospatial-vector-veracity/vector_data/point/alberta_point_locations.csv')
-british_columbia = pd.read_csv('../geospatial-vector-veracity/vector_data/point/british_columbia_point_locations.csv')
-manitoba = pd.read_csv('../geospatial-vector-veracity/vector_data/point/manitoba_point_locations.csv')
-nwt = pd.read_csv('../geospatial-vector-veracity/vector_data/point/northwest_territories_point_locations.csv')
-saskatchewan = pd.read_csv('../geospatial-vector-veracity/vector_data/point/saskatchewan_point_locations.csv')
-yukon = pd.read_csv('../geospatial-vector-veracity/vector_data/point/yukon_point_locations.csv')
+alaska = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/alaska_point_locations.csv"
+)
+alberta = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/alberta_point_locations.csv"
+)
+british_columbia = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/british_columbia_point_locations.csv"
+)
+manitoba = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/manitoba_point_locations.csv"
+)
+nwt = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/northwest_territories_point_locations.csv"
+)
+saskatchewan = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/saskatchewan_point_locations.csv"
+)
+yukon = pd.read_csv(
+    "../geospatial-vector-veracity/vector_data/point/yukon_point_locations.csv"
+)
 
-all_locations = pd.concat([
-    alaska,
-    alberta,
-    british_columbia,
-    manitoba,
-    nwt,
-    saskatchewan,
-    yukon
-])
+all_locations = pd.concat(
+    [alaska, alberta, british_columbia, manitoba, nwt, saskatchewan, yukon]
+)
 
-non_nwt_locations = pd.concat([
-    alaska,
-    alberta,
-    british_columbia,
-    manitoba,
-    saskatchewan,
-    yukon
-])
+non_nwt_locations = pd.concat(
+    [alaska, alberta, british_columbia, manitoba, saskatchewan, yukon]
+)
 
-scenarios_lu = [
-    'cru32',
-    'prism',
-    'rcp45',
-    'rcp60',
-    'rcp85'
-]
+scenarios_lu = ["cru32", "prism", "rcp45", "rcp60", "rcp85"]
 
 communities_lu = {
-    'cru32': all_locations,
-    'prism': non_nwt_locations,
-    'rcp45': all_locations,
-    'rcp60': all_locations,
-    'rcp85': all_locations
+    "cru32": all_locations,
+    "prism": non_nwt_locations,
+    "rcp45": all_locations,
+    "rcp60": all_locations,
+    "rcp85": all_locations,
 }
 
-types_lu = {
-    'tas': 'Temperature',
-    'pr': 'Precipitation'
-}
+types_lu = {"tas": "Temperature", "pr": "Precipitation"}
 
 resolutions_lu = {
-    'cru32': [
-        '10min'
-    ],
-    'prism': [
-        '2km'
-    ],
-    'rcp45': [
-        '2km',
-        '10min'
-    ],
-    'rcp60': [
-        '2km',
-        '10min'
-    ],
-    'rcp85': [
-        '2km',
-        '10min'
-    ],
+    "cru32": ["10min"],
+    "prism": ["2km"],
+    "rcp45": ["2km", "10min"],
+    "rcp60": ["2km", "10min"],
+    "rcp85": ["2km", "10min"],
 }
 
 dateranges_lu = {
-    'cru32': [
-        [1960, 1989]
-    ],
-    'prism': [
-        [1961, 1990]
-    ],
-    'rcp45': [
-        [2040, 2049],
-        [2060, 2069],
-        [2090, 2099]
-    ],
-    'rcp60': [
-        [2040, 2049],
-        [2060, 2069],
-        [2090, 2099]
-    ],
-    'rcp85': [
-        [2040, 2049],
-        [2060, 2069],
-        [2090, 2099]
-    ]
+    "cru32": [[1960, 1989]],
+    "prism": [[1961, 1990]],
+    "rcp45": [[2040, 2049], [2060, 2069], [2090, 2099]],
+    "rcp60": [[2040, 2049], [2060, 2069], [2090, 2099]],
+    "rcp85": [[2040, 2049], [2060, 2069], [2090, 2099]],
 }
 
 projections_lu = {
-    'cru32': 'EPSG:4326',
-    'prism': 'EPSG:3338',
-    'rcp45': 'EPSG:3338',
-    'rcp60': 'EPSG:3338',
-    'rcp85': 'EPSG:3338'
+    "cru32": "EPSG:4326",
+    "prism": "EPSG:3338",
+    "rcp45": "EPSG:3338",
+    "rcp60": "EPSG:3338",
+    "rcp85": "EPSG:3338",
 }
